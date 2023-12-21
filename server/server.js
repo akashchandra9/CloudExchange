ceb4aa27ab36de7bb2caf0a7799bd9a57e2fe9ff
akashchandra9 <114898209+akashchandra9@users.noreply.github.com>
2023-12-21 21:38:02 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,13 +4,13 @@ const path = require("path");
 app.use(express.static(path.join(__dirname, "build")));
 app.use(function(req, res, next) {
   // res.header("Access-Control-Allow-Origin", "*");
-  const allowedOrigins = ['http://localhost:3000', 'http://192.168.219.249', 'https://sky-wcpy.onrender.com'];
+  const allowedOrigins = ['http://localhost:3000', 'http://192.168.219.249', "https://skyboxe.onrender.com"];
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
        res.setHeader('Access-Control-Allow-Origin', origin);
   }
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
   next();
 });
